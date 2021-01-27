b09f4a4576bcd0314f9c448b48e4d29acffaaf71
Marcin Putra <marcin.putra@gmail.com>
2021-01-27 14:35:45 +0100

    added "adding layout"

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,32 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.beans.property.ReadOnlyIntegerProperty;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Stage stage = new Stage();
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+
+        GridPane gridPane = new GridPane();
+        Scene scene = new Scene(gridPane, 800, 600);
+        stage.setScene(scene);
+        stage.setTitle("Book DataBase Control");
+        stage.show();
+
+        addingLayout();
     }
 
 
@@ -22,6 +35,49 @@ public class Main extends Application {
     }
 
     public void addingLayout(){
+
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setVgap(30);
+        root.setHgap(30);
+
+        Text title = new Text("Title");
+        TextField enterTitle = new TextField();
+        enterTitle.setTooltip(new Tooltip("enter the title"));
+
+        Text pages = new Text("Pages");
+        TextField enterPages = new TextField();
+        enterPages.setTooltip(new Tooltip("if left empty the value assigned will be 0"));
+
+
+        Button add = new Button("Add to database");
+        add.setOnAction(event -> {
+            if (enterTitle.getText().isEmpty()){
+                Alert alert = new Alert(Alert.AlertType.ERROR, "No title entered", ButtonType.CLOSE);
+                alert.show();
+            }
+            else if (enterPages.getText().isEmpty()){
+                Book book = new Book(enterTitle.getText(), 0);
+                DbController.addToDB(book);
+            }
+            else{
+                Book book = new Book(enterTitle.getText(), Integer.parseInt(enterPages.getText()));
+                DbController.addToDB(book);
+            }
+        });
+        Button goToTableLayout = new Button("view table");
+        goToTableLayout.setOnAction(event -> {
+            tableLayout();
+        });
+
+        root.add(title, 0, 0);
+        root.add(enterTitle, 1, 0);
+        root.add(pages, 0, 1);
+        root.add(enterPages, 1, 1);
+        root.add(add, 1, 3);
+        root.add(goToTableLayout, 1, 4);
+
+        stage.getScene().setRoot(root);
 
     }
 
